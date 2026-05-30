8ab38a607901655d41d7a26b64e3ff566b4c1300
xianmingxuan <1559779311@qq.com>
2026-05-30 22:15:06 +0800

    引入 Addressable包

---
+++ Assets/Script/Game/PlayerSpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using QFramework;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace UG20260527
+{
+    public class PlayerSpawnPoint : MonoBehaviour, IController
+    {
+        [Tooltip("玩家资源 弱引用")]
+        public AssetReference PlayerPrefab;
+
+        async void Start()
+        {
+            GameObject pre = await PlayerPrefab.LoadAssetAsync<GameObject>().Task;
+            GameObject obj = Instantiate(pre, transform.position, transform.rotation, transform.parent);
+        }
+
+        IArchitecture IBelongToArchitecture.GetArchitecture()
+        {
+            return UnityGame20260527.Interface;
+        }
+    }
+}
